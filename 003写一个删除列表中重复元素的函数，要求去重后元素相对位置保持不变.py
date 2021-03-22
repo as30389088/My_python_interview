797760259b5df4fdb78db629934295b98e2964bc
63038 <wangyuelei1226@gmail.com>
2021-03-23 00:22:01 +0800

    003 列表去重，并保留相对位置

--- 003写一个删除列表中重复元素的函数，要求去重后元素相对位置保持不变.py
+++ 003写一个删除列表中重复元素的函数，要求去重后元素相对位置保持不变.py
@@ -28,3 +28,9 @@ print(dup_func(test_list))
 print(list(dup_yield_func(test_list)))
 print(dup_yield_func(test_list))
 
+"""
+扩展：由于Python中的集合底层使用哈希存储，所以集合的in和not in成员运算在性能上远远优于列表，
+所以上面的代码我们使用了集合来保存已经出现过的元素。集合中的元素必须是hashable对象，
+因此上面的代码在列表元素不是hashable对象时会失效，要解决这个问题可以给函数增加一个参数，
+该参数可以设计为返回哈希码或hashable对象的函数。
+"""
